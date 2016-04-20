2895806c819d004c3e1d20117297825507c1dc7c
Benjamin Dieter <Benjamin.Dieter@jobframe.de>
2016-04-20 14:24:31 +0200

    Initial commit of extremely simple webserver

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-"# rgb-pi-js" 
+## rgb-pi-js
+
+JavaScript / NodeJS port of [rgb-pi](https://github.com/ryupold/rgb-pi)
+
+## Code Example
+
+--
+
+## Motivation
+
+--
+
+## Installation
+
+--
+
+## API Reference
+
+--
+
+## Tests
+
+--
+
+## Contributors
+
+--
+
+## License
+
+--
---
+++ server.js
@@ -0,0 +1,48 @@
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Require the dispatcher
+var dispatcher = require('httpdispatcher');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    try {
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
+
+//A sample GET request    
+dispatcher.onGet("/cc", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+	
+	var color = req.params.c || false;
+	//Do logic
+	if (color)
+		console.log("Set color: " + color);
+	
+    res.end('Page One');
+});    
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
