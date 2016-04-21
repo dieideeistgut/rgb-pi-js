4c4afa050a0b99002b616df2c4dc9531efa272ef
Benjamin Dieter <Benjamin.Dieter@jobframe.de>
2016-04-21 09:48:35 +0200

    Did some intendation

--- color.js
+++ color.js
@@ -57,12 +57,12 @@ function hexStringToFloatArray(hexString) {
 	assert(hexString.length === 3 || hexString.length === 6, "Hex string must be 3 or 6 characters long");
 	
 	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hexString = hexString.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	hexString = hexString.replace(shorthandRegex, function(m, r, g, b) {
+		return r + r + g + g + b + b;
+	});
 
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexString);
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexString);
 	
 	return byteArrayToFloatArray([parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)]);
 }
